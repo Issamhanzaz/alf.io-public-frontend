50f73977aa9d9616726b38cac33d5b7b2ddd465c
Issam Hanzaz <413256@student.fontys.nl>
2020-05-26 04:17:25 +0200

    small css fixes

--- src/app/event-list/event-list.component.ts
+++ src/app/event-list/event-list.component.ts
@@ -9,41 +9,6 @@ import { AnalyticsService } from '../shared/analytics.service';
 import { InfoService } from '../shared/info.service';
 import { zip } from 'rxjs';
 
-interface Country {
-  name: string;
-  flag: string;
-  area: number;
-  population: number;
-}
-
-const COUNTRIES: Country[] = [
-  {
-    name: 'Russia',
-    flag: 'f/f3/Flag_of_Russia.svg',
-    area: 17075200,
-    population: 146989754
-  },
-  {
-    name: 'Canada',
-    flag: 'c/cf/Flag_of_Canada.svg',
-    area: 9976140,
-    population: 36624199
-  },
-  {
-    name: 'United States',
-    flag: 'a/a4/Flag_of_the_United_States.svg',
-    area: 9629091,
-    population: 324459463
-  },
-  {
-    name: 'China',
-    flag: 'f/fa/Flag_of_the_People%27s_Republic_of_China.svg',
-    area: 9596960,
-    population: 1409517397
-  }
-];
-
-
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
@@ -51,8 +16,6 @@ const COUNTRIES: Country[] = [
 
 })
 export class EventListComponent implements OnInit {
-  countries = COUNTRIES;
-
   events: Array<BasicEventInfo[]>;
   languages: Language[];
 
